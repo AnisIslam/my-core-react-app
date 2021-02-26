f755ae48c3699c38074a2523f9a2c92ccfcd847b
Anis Islam <manisul1997bd@gmail.com>
2021-02-26 22:33:25 +0600

    second commit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,18 @@
 
 import './App.css';
 import React, { useEffect, useState } from 'react';
+import Header from './components/Header/Header';
+import Shop from './components/Shop/Shop';
+
 
 function App() {
   
   return (
     <div>
+      <Header></Header>
+      <Shop></Shop>
+      
      
-        
-
-       
-
-    
 
     </div>
   );
